cc708ea3996d93f745065cc38782a13f45e87067
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2026-09-14 17:57:06 +0100

    existing code (manual fork)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Modified-Missingness-Sim-Study
-Modified version of the missingness simulation study, which seeks to compare variable selection methods and missing data handling methods.
+Simulation study comparing variable selection and missing data handling methods.
---
+++ bash/download_results.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# ****************************************
+# Missingness Simulation Study
+#
+# BluePebble Automation Script
+# Downloads results from HPC to local
+# 
+# Emma Tarmey
+#
+# Started:          06/10/2025
+# Most Recent Edit: 10/11/2025
+# ****************************************
+
+# pull entire repo from BP to local
+scp -r aa22294@bp1-login01b.acrc.bris.ac.uk:~/Missingness-Sim-Study "/home/aa22294/Desktop/PhD/Projects/Missingness-Sim-Study/"
+
